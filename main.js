5b48c49a8043b5f3aeae6fa1f7ec5db7b9b5ecc4
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-09-09 00:36:41 -0400

    grid layout and first JS logic

---
+++ main.js
@@ -0,0 +1,9 @@
+// variables
+const displayScreen = document.getElementById("displayScreen");
+const clearBtn = document.getElementById("clear-btn");
+const equalsBtn = document.getElementById("equals-btn");
+const numBtns = document.getElementsByClassName("number");
+
+//get value from numbers buttons(on click) to display on screen
+//eval expression
+//clear display screen
