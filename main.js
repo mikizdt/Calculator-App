8ff0be599af3acf1083f14733b4bcee9da98b279
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-09-12 02:01:17 -0400

    just playing with code

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// variables
-
+//variables
 const displayScreen = document.getElementById("display-screen");
 const clearBtn = document.getElementById("clear-btn");
 const equalsBtn = document.getElementById("equals-btn");
@@ -8,11 +7,13 @@ const calculatorBtns = document.getElementsByTagName("button");
 //get value from numbers buttons(on click) to display on screen
 for (i = 0; i < calculatorBtns.length; i++) {
   calculatorBtns[i].addEventListener("click", function () {
-    displayScreen.innerText += this.innerText;
+    displayScreen.innerText += this.value;
   });
 }
 
 //eval expression
+equalsBtn.addEventListener("click", function () {});
+
 //clear display screen
 clearBtn.addEventListener("click", function () {
   displayScreen.innerText = "";
